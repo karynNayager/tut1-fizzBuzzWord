98fd3b56c0e36c702f4bab78be208dd37545d43d
karynnayager <karynnayager.kn@gmail.com>
2016-02-19 12:30:41 +0200

    woof

--- karyn tut1.cpp
+++ karyn tut1.cpp
@@ -6,19 +6,35 @@ int main()
 {
 	int i;
 	
-	for (i = 1; i <= 20; i++)
+	for (i = 1; i <= 50; i++)
 	{
-		if (i % 3 == 0 && i%5==0)
+		if (i % 3 == 0 && i%5==0 && i%7==0)
+		{
+			cout << "fizzbuzzwoof" << endl;
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			cout << "fizzbuzz" << endl;
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 7== 0)
+		{
+			cout << "fizzwoof" << endl;
+		}
+		else if (i % 5 == 0 && i % 7 == 0)
+		{
+			cout << "buzzwoof" << endl;
+		}
+		else if (i % 3 == 0 )
 		{
 			cout << "fizz" << endl;
 		}
 		else if (i % 5 == 0)
 		{
 			cout << "buzz" << endl;
+		}
+		else if (i % 7 == 0)
+		{
+			cout << "woof" << endl;
 		}
 		else
 		{
